303a1a6eb2c83027b8ff639ab224ed5e104d3f7b
Robert Beier <30528168+Robert-Beier@users.noreply.github.com>
2020-04-25 15:06:59 +0200

    Refactor code to smaller functions

--- src/injected.js
+++ src/injected.js
@@ -1,16 +1,15 @@
 'use strict';
 
-function exportCSV() {
-    if (!window.APP || !window.APP.conference || !window.APP.conference.roomName) {
-        alert('Sie müssen sich in einem Meeting Raum befinden.');
-        return;
+(() => {
+    function getHeaderRows(roomName, currentDate) {
+        return [
+            ["Raum Name:", roomName],
+            ["Zeitpunkt des Exports:", currentDate.toLocaleString()],
+            [],
+            ["Teilnehmer", "Sprechzeit"]
+        ];
     }
-    const speakerStats = window.APP.conference.getSpeakerStats();
-    const currentDate = new Date();
-    const roomName = window.APP.conference.roomName;
-
-    const speakerRows = Object.values(speakerStats).map(speakerDetails => {
-        const name = speakerDetails.displayName || 'Unbekannt';
+    function getTimeStringForSpeaker(speakerDetails) {
         const dominantTime = speakerDetails.totalDominantSpeakerTime;
         const dominantTimeDate = new Date(dominantTime);
         let timeString = '';
@@ -20,29 +19,45 @@ function exportCSV() {
             const numberOfDays = Math.floor(86400000 / 1000 / 60 / 60 / 24);
             timeString = numberOfDays + ' Tage ' + dominantTimeDate.toLocaleTimeString('de-DE', { timeZone: 'UTC' });
         }
-        return [name, timeString];
-    });
+        return timeString;
+    }
+    function getSpeakerRows(speakerStats) {
+        return Object.values(speakerStats).map(speakerDetails => {
+            const name = speakerDetails.displayName || 'Unbekannt';
+            const timeString = getTimeStringForSpeaker(speakerDetails);
+            return [name, timeString];
+        });
+    }
+    function getRows(speakerStats, roomName, currentDate) {
+        const speakerRows = getSpeakerRows(speakerStats);
+        const headerRows = getHeaderRows(roomName, currentDate);
+        return [...headerRows, ...speakerRows];
+    }
+    function getEncodedUri(rows) {
+        const csvContent = "data:text/csv;charset=utf-8,"
+            + rows.map(e => e.join(";")).join("\n");
+        return encodeURI(csvContent);
+    }
+    function getFileName(roomName, currentDate) {
+        return roomName + '_' + currentDate.toISOString().split('T')[0] + ".csv";
+    }
+    function downloadCSVFile(fileName, encodedUri) {
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+    }
 
-    const headerRows = [
-        ["Raum Name:", roomName],
-        ["Zeitpunkt des Exports:", currentDate.toLocaleString()],
-        [],
-        ["Teilnehmer", "Sprechzeit"]
-    ];
-
-    const rows = [...headerRows, ...speakerRows];
-
-    let csvContent = "data:text/csv;charset=utf-8,"
-        + rows.map(e => e.join(";")).join("\n");
-    var encodedUri = encodeURI(csvContent);
-
-    const fileName = roomName + '_' + currentDate.toISOString().split('T')[0] + ".csv";
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", fileName);
-    document.body.appendChild(link);
-
-    link.click();
-}
-
-exportCSV();
+    if (!window.APP || !window.APP.conference || !window.APP.conference.roomName) {
+        alert('Sie müssen sich in einem Meeting Raum befinden.');
+        return;
+    }
+    const speakerStats = window.APP.conference.getSpeakerStats();
+    const currentDate = new Date();
+    const roomName = window.APP.conference.roomName;
+    const rows = getRows(speakerStats, roomName, currentDate);
+    const encodedUri = getEncodedUri(rows);
+    const fileName = getFileName(roomName, currentDate);
+    downloadCSVFile(fileName, encodedUri);
+})();
